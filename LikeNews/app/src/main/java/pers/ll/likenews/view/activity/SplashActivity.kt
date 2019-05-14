65e6b7b40da6aec1c4c14c788c6eb63170548bbc
RookieJay <1475485144@qq.com>
2019-05-14 12:17:29 +0800

    闪屏页今日古诗

---
+++ LikeNews/app/src/main/java/pers/ll/likenews/view/activity/SplashActivity.kt
@@ -0,0 +1,114 @@
+package pers.ll.likenews.view.activity
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import com.jinrishici.sdk.android.JinrishiciClient
+import com.jinrishici.sdk.android.listener.JinrishiciCallback
+import com.jinrishici.sdk.android.model.JinrishiciRuntimeException
+import com.jinrishici.sdk.android.model.PoetySentence
+import kotlinx.android.synthetic.main.activity_music_play.*
+import pers.ll.likenews.R
+import kotlinx.android.synthetic.main.activity_splash.*
+import kotlinx.android.synthetic.main.header_layout_drawer_navigation.*
+import pers.ll.likenews.consts.Const
+import pers.ll.likenews.utils.ImageUtil
+import pers.ll.likenews.utils.MainHandler
+import pers.ll.likenews.utils.ThreadPoolManager
+import java.lang.ref.WeakReference
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var mHandler : DelayHandler
+    private var executor = ThreadPoolManager.getInstance()
+    private var imageUtil = ImageUtil.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        mHandler = DelayHandler(this)
+        initView()
+        countDown()
+        setListener()
+    }
+
+    private fun initView() {
+//        flSplash.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary))
+    }
+
+    private fun countDown() {
+        val client = JinrishiciClient()
+        client.getOneSentenceBackground(object : JinrishiciCallback {
+            override fun done(poetySentence: PoetySentence?) {
+                if (poetySentence != null && poetySentence.data != null) {
+                    val content = poetySentence.data.content
+                    val sentences = content.split("，")
+                    val sentence1 = filterMark(sentences[0])
+                    val sentence2 = sentences[1].replace("。", "")
+                    tvSentence1.text = sentence1
+                    tvSentence2.text = sentence2
+                }
+            }
+
+            override fun error(e: JinrishiciRuntimeException?) {
+                Log.d("poetry return error", e?.code.toString() + e?.message)
+            }
+
+        })
+        executor.execute {
+            for (i in 5 downTo 0) {
+                val msg = mHandler.obtainMessage()
+                msg.what = 1
+                msg.arg1 = i
+                mHandler.sendMessage(msg)
+                try {
+                    Thread.sleep(1000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+            val bitmap = imageUtil.url2BitMap(Const.URL.BING_DAILY_PIC)
+            if (bitmap != null) {
+                //启用高斯模糊
+                val blurBitmap = imageUtil.rsBlur(flSplash.context, bitmap, 24, 1f / 8f)
+                MainHandler.getInstance().post {
+                    flSplash.background = imageUtil.getDrawbleFormBitmap(flSplash.context, bitmap) }
+            }
+        }
+    }
+
+    private fun filterMark(str : String) : String{
+        val marks = "/[\\u3002|\\uff1f|\\uff01|\\uff0c|\\u3001|\\uff1b|\\uff1a|\\u201c|\\u201d|\\u2018|\\u2019|\\uff08|\\uff09|\\u300a|\\u300b|\\u3008|\\u3009|\\u3010|\\u3011|\\u300e|\\u300f|\\u300c|\\u300d|\\ufe43|\\ufe44|\\u3014|\\u3015|\\u2026|\\u2014|\\uff5e|\\ufe4f|\\uffe5]/"
+        return str.replace(marks, "")
+    }
+
+    private fun setListener() {
+        tv_skip.setOnClickListener {
+            startActivity(Intent(this, MainActivity().javaClass))
+            finish()
+        }
+    }
+
+    class DelayHandler(splashActivity: SplashActivity) : Handler() {
+
+        private val weakReference = WeakReference<SplashActivity>(splashActivity)
+
+        override fun handleMessage(msg: Message) {
+            val activity = weakReference.get()
+            if (activity != null) {
+                when (msg.what) {
+                    1 -> if (msg.arg1 == 0) {
+                        activity.startActivity(Intent(activity.applicationContext, MainActivity().javaClass))
+                        activity.finish()
+                    } else {
+                        activity.tv_skip.text = String.format("跳过%s秒", msg.arg1.toString())
+                    }
+                }
+            }
+        }
+    }
+}
--- LikeNews/app/src/main/java/pers/ll/likenews/view/fragment/PersonalFragment.kt
+++ LikeNews/app/src/main/java/pers/ll/likenews/view/fragment/PersonalFragment.kt
@@ -1,23 +1,23 @@
 package pers.ll.likenews.view.fragment
 
-import android.widget.TextView
-import com.jinrishici.sdk.android.JinrishiciClient
-import com.jinrishici.sdk.android.api.JinrishiciAPI
 import pers.ll.likenews.R
 import pers.ll.likenews.base.BaseFragment
+import pers.ll.likenews.ui.MyTextView
 
 class PersonalFragment : BaseFragment() {
 
-    private lateinit var tvTest : TextView
+    private lateinit var tvTitle : MyTextView
 
     override fun setContentView(): Int {
         return R.layout.fragment_personal
     }
 
     override fun initView() {
+        tvTitle = findViewById(R.id.tvTitle) as MyTextView
+        tvTitle.text = resources.getString(R.string.txt_poetry)
     }
 
     override fun loadData() {
-        val client = JinrishiciClient()
+
     }
 }
